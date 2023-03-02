e2a660d05766c0b80f4ead85b200822df5712c8e
Shane Arland <arlandshane@gmail.com>
2023-03-02 15:47:05 +0530

    changed port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const port = 3000
+const Port = process.env.Port || 3000
 
 const app = express()
 app.set('view engine', 'ejs')
@@ -50,6 +50,6 @@ app.post('/', async (req, res) => {
     }
 })
 
-app.listen(port, () => {
+app.listen(Port, () => {
     console.log('Server started on port 3000')
 })
