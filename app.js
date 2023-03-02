a060ac68f74221fb34eaa6b4a483c68d058fa1eb
Shane Arland <arlandshane@gmail.com>
2023-03-02 16:13:29 +0530

    test commit 2

--- app.js
+++ app.js
@@ -1,70 +1,87 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const { MongoClient } = require('mongodb');
-const app = express()
-const port = process.env.Port || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static('public'))
-
+// Connect to MongoDB using MongoClient
 const uri = process.env.MONGO_CONNECTION_STRING;
 const client = new MongoClient(uri);
 
-client.connect(err => {
+// Connect to default database using Mongoose
+mongoose.connect(uri + '/my_db');
+
+// Define the items schema
+const itemsSchema = {
+    name: String,
+};
+
+const Item = mongoose.model('Item', itemsSchema);
+
+const item1 = new Item({
+    name: 'default item',
+});
+
+const defaultItems = [];
+
+Item.insertMany(defaultItems);
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', async (req, res) => {
+    try {
+        const foundItems = await Item.find({}, 'name');
+        const itemNames = foundItems.map((item) => item.name);
+
+        res.render('list', {
+            kindOfDay: 'Enter your name in the database',
+            newListItems: itemNames,
+        });
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.post('/', async (req, res) => {
+    const newItem = new Item({
+        name: req.body.newItem,
+    });
+
+    defaultItems.push(newItem);
+
+    try {
+        await newItem.save();
+        res.redirect('/');
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.get('/items/:my_item', async (req, res) => {
+    const my_item = req.params.my_item;
+
+    try {
+        const item = await client
+            .db('my_db')
+            .collection('my_collection')
+            .find({}, 'name');
+
+        return res.json(item);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+client.connect((err) => {
     if (err) {
         console.error(err);
         return false;
     }
-    console.log('Connected to MongoDB Atlas');
-
-    const itemsSchema = {
-        name: String
-    }
-
-    const Item = mongoose.model('Item', itemsSchema)
-
-    const item1 = new Item({
-        name: 'default item'
-    })
-
-    const defaultItems = []
-
-    Item.insertMany(defaultItems)
-
-    app.get('/', async (req, res) => {
-        try {
-            const foundItems = await client.db("my_db")
-                .collection("my_collection")
-                .find({}, 'name')
-                .toArray();
-
-            const itemNames = foundItems.map(item => item.name)
-            res.render('list', { kindOfDay: 'Enter your name in the database', newListItems: itemNames })
-        } catch (err) {
-            console.log(err)
-        }
-    })
-
-    app.post('/', async (req, res) => {
-        const newItem = new Item({
-            name: req.body.newItem
-        })
-
-        defaultItems.push(newItem)
-
-        try {
-            await client.db("my_db")
-                .collection("my_collection")
-                .insertOne(newItem);
-            res.redirect('/')
-        } catch (err) {
-            console.log(err)
-        }
-    })
-
+    // connection to mongo is successful, listen for requests
     app.listen(port, () => {
-        console.log(`Server started on port ${port}`)
-    })
-})
+        console.log('listening for requests');
+    });
+});
